3d9567b0b8cabac6dd9ffd4892a44a520706f215
mSebbi <sebastianonasti49@gmail.com>
2021-09-27 22:58:20 +0200

    Hauptmenü
    -Hauptmenü hinzugefügt
    -Man kann das Spiel starten und verlassen über das hauptmenü

---
+++ 3D Platformer/Main_Menu.gd
@@ -0,0 +1,42 @@
+extends MarginContainer
+
+onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer3/HBoxContainer/Cursor
+onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer4/HBoxContainer2/Cursor2
+
+var current_selection = 0
+
+const erstes_level = preload("res://World.tscn")
+
+func _ready():
+	set_current_selection(0)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("backward") and current_selection < 1:
+		current_selection += 1
+		set_current_selection(current_selection)
+		
+	elif Input.is_action_just_pressed("forward") and current_selection > 0:
+		current_selection -= 1
+		set_current_selection(current_selection)
+		
+	elif Input.is_action_just_pressed("ui_accept"):
+		handle_selection(current_selection)
+
+
+func handle_selection(current_selection):
+	if current_selection == 0:
+		get_parent().add_child(erstes_level.instance())
+		queue_free()
+		
+	if current_selection == 1:
+		get_tree().quit()
+
+func set_current_selection(_current_selection):
+	selector_one.text = ""
+	selector_two.text = ""
+	
+	if _current_selection == 0:
+		selector_one.text = "-->"
+	elif _current_selection == 1:
+		selector_two.text = "-->"
+		
